b04ae05fcbd5701a785fcdc8d1eec1be9a2c2feb
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-25 23:02:56 +0000

    0x00. MySQL advanced - Mandatory tasks 0 & 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,7 +6,7 @@
 -- If the table already exists, the script will not fail
 -- The script can be executed on any database
 CREATE TABLE IF NOT EXISTS `users` (
-    id INT  NOT NULL  AUTO INCREMENT  PRIMARY KEY,
+    id INT  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
     email VARCHAR(255)  NOT NULL  UNIQUE,
     name VARCHAR(255)
 )
