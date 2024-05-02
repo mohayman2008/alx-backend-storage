07ead7346f0a781996bf518b033cbe39d3cc97b4
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-02 07:38:03 +0000

    0x02. Redis basic - Task 5 - Refactoring>

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,34 @@
 #!/usr/bin/env python3
-"""In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:). The core of
-the function is very simple. It uses the requests module
-to obtain the HTML content of a particular URL and returns it.
+'''This module contains the definition of the function "get_page"'''
+from datetime import timedelta
+# from functools import wraps
 
-Start in a new file named web.py and do not reuse the code
-written in exercise.py.
-
-Inside get_page track how many times a particular URL was
-accessed in the key "count:{url}" and cache the result with
-an expiration time of 10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to simulate
-a slow response and test your caching."""
-
-
-import redis
+from redis import Redis
 import requests
-from functools import wraps
 
-r = redis.Redis()
-
-
-def url_access_count(method):
-    """decorator for get_page function"""
-    @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
-        return html_content
-    return wrapper
+# redis = Redis()
 
 
-@url_access_count
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
+    '''get_page: uses <requests> module to obtain the HTML content of <url>,
+    track how many times it was accessed and cache the access count with the
+    key "count:{url}" and an expiration time of 10 seconds.
+    It returns the requested HTML content
+
+    url: the url to be requested
+    '''
+    with Redis() as redis:
+        cache = redis.get(url)
+        if cache:
+            res = cache.decode("utf-8")
+        else:
+            res = requests.get(url).text
+            redis.setx(url, res, timedelta(seconds=10))
+
+        key = "count:" + url
+        redis.incr(key)
+
+    return res
 
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+# redis.quit()
