4e42fc89d8ef00d1991a67f2b550c72903de4f20
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-02 01:37:18 +0000

    0x02. Redis basic - Task 0 - Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''This module contains the definition of the class "Cache"'''
-from typing import Union
+from typing import Optional, Union
 from uuid import uuid4
 
 import redis
@@ -14,11 +14,13 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> Optional[str]:
         '''Stores <data> in Redis using randomly generated key
         and returns that key'''
-        key = str(uuid4())
+        if type(data) not in [str, bytes, int, float]:
+            return None
 
+        key = str(uuid4())
         self._redis.set(key, data)
 
         return key
